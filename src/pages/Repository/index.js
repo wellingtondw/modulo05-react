708761d3f78bf4ab64fa4e791d692329a26c6188
Wellington Lima <wellingtonslpbi@hotmail.com>
2020-01-24 20:59:44 -0300

    list issues and issues labels

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import api from '../../services/api';
-import { Loading, Owner } from './styles';
+import { Loading, Owner, IssuesList } from './styles';
 import Container from '../../components/container';
 
 export default function Repository({ match }) {
@@ -42,6 +42,25 @@ export default function Repository({ match }) {
         <h1>{repository.name}</h1>
         <p>{repository.description}</p>
       </Owner>
+
+      <IssuesList>
+        {issues.map(issue => (
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login} />
+            <div>
+              <strong>
+                <a href={issue.html_url} target="_blank">
+                  {issue.title}
+                </a>
+                {issue.labels.map(label => (
+                  <span key={String(label.id)}>{label.name}</span>
+                ))}
+              </strong>
+              <p>{issue.user.login}</p>
+            </div>
+          </li>
+        ))}
+      </IssuesList>
     </Container>
   );
 }
