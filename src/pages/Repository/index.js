3c64b7d99e7705e73d4b05bc607011a76acac430
Wellington Lima <wellingtonslpbi@hotmail.com>
2020-01-24 09:16:46 -0300

    loading data from repository and issues

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,32 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api';
 // import { Container } from './styles';
 
 export default function Repository({ match }) {
-  return <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>;
+  const [repository, setRepository] = {};
+  const [issues, setIssues] = [];
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadingGithubData() {
+      const repoName = decodeURIComponent(match.params.repository);
+
+      const [repository, issues] = await Promise.all([
+        api.get(`/repos/${repoName}`),
+        api.get(`/repos/${repoName}/issues`, {
+          params: {
+            state: 'open',
+            per_page: 5,
+          },
+        }),
+      ]);
+
+      setRepository(repository.data);
+      setIssues(issues.data);
+      setLoading(false);
+    }
+    loadingGithubData();
+  }, []);
+
+  return <h1>Repository</h1>;
 }
